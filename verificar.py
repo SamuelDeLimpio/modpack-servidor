e180b7f6ed7263ae9d15d7cc03bfdef812fd4517
samueldelimpio <samueldelimpio@gmail.com>
2026-03-20 19:21:07 -0300

    Update .gitattributes and verificar.py to include .mcmeta extension and improve hash normalization process

--- verificar.py
+++ verificar.py
@@ -1,10 +1,11 @@
 import os
 
 # Extensiones de texto que están dando problemas de hash
-extensions = ('.cfg', '.conf', '.csv', '.meta', '.json', '.json5', '.properties', '.pw.toml', '.toml', '.txt')
-folders = ['config', 'mods', 'resourcepacks']
+extensions = ('.cfg', '.conf', '.csv', '.meta', '.json', '.json5', '.properties', '.pw.toml', '.toml', '.txt', '.mcmeta')
+folders = ['config', 'mods', 'resourcepacks', 'notfound']
 
 for folder in folders:
+    if not os.path.exists(folder): continue
     for root, dirs, files in os.walk(folder):
         for file in files:
             if file.endswith(extensions):
@@ -12,11 +13,11 @@ for folder in folders:
                 with open(path, 'rb') as f:
                     content = f.read()
                 
-                # Convertir CRLF a LF
+                # Convertir CRLF (\r\n) a LF (\n)
                 new_content = content.replace(b'\r\n', b'\n')
                 
                 with open(path, 'wb') as f:
                     f.write(new_content)
                 print(f"Normalizado: {path}")
 
-print("\n--- Limpieza de .csv, .conf y .meta completada ---")
+print("\n--- ¡Limpieza completada! Todos los archivos son ahora compatibles con GitHub ---")
